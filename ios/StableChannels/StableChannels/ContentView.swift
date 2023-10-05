c3713553b418bc19bafad8b3ab4a9c467e11510f
toneloc <@tonklaus>
2023-10-05 10:12:48 -0400

    added price

--- ios/StableChannels/StableChannels/ContentView.swift
+++ ios/StableChannels/StableChannels/ContentView.swift
@@ -3,6 +3,7 @@ import Combine
 
 struct ContentView: View {
     @State private var balance: Double = 0.00
+    @State private var bitcoinPrice: Double = 0.00
     @State private var lastUpdated: Date?
     @State private var cancellable: AnyCancellable?
     @State private var showMenu: Bool = false
@@ -35,32 +36,48 @@ struct ContentView: View {
                 
             Text(String(format: "$%.2f", balance))
                 .font(.largeTitle)
-                
-            if let lastUpdated = lastUpdated {
-                Text("Last Updated: \(formattedDate(from: lastUpdated))")
+            
+            VStack(spacing: 5) {
+                Text("Bitcoin Price: \(String(format: "$%.2f", bitcoinPrice))")
                     .font(.caption)
                     .foregroundColor(.gray)
+                            
+                if let lastUpdated = lastUpdated {
+                    Text("Last Updated: \(formattedDate(from: lastUpdated))")
+                        .font(.caption)
+                        .foregroundColor(.gray)
+                }
             }
 
-            HStack(spacing: 50) {
+            HStack(spacing: 20) {
                 Button(action: {
                     self.balance -= 1.00
                 }) {
-                    Text("Send")
-                        .padding()
-                        .background(Color.blue)
-                        .foregroundColor(.white)
-                        .cornerRadius(10)
+                    HStack {
+                        Image(systemName: "arrow.up.right.square.fill")
+                        Text("Send")
+                    }
+                    .padding()
+                    .frame(width: 150, height: 50)
+                    .background(LinearGradient(gradient: Gradient(colors: [Color.blue, Color.blue.opacity(0.7)]), startPoint: .top, endPoint: .bottom))
+                    .foregroundColor(.white)
+                    .cornerRadius(25)
+                    .shadow(color: Color.blue.opacity(0.4), radius: 10, x: 0, y: 10)
                 }
 
                 Button(action: {
                     self.balance += 1.00
                 }) {
-                    Text("Receive")
-                        .padding()
-                        .background(Color.gray)
-                        .foregroundColor(.white)
-                        .cornerRadius(10)
+                    HStack {
+                        Image(systemName: "arrow.down.left.square.fill")
+                        Text("Receive")
+                    }
+                    .padding()
+                    .frame(width: 150, height: 50)
+                    .background(LinearGradient(gradient: Gradient(colors: [Color.gray, Color.gray.opacity(0.7)]), startPoint: .top, endPoint: .bottom))
+                    .foregroundColor(.white)
+                    .cornerRadius(25)
+                    .shadow(color: Color.gray.opacity(0.4), radius: 10, x: 0, y: 10)
                 }
             }
             
@@ -69,6 +86,7 @@ struct ContentView: View {
         .padding()
         .onAppear {
             fetchBalance()
+            print("fetching")
             Timer.scheduledTimer(withTimeInterval: 30.0, repeats: true) { _ in
                 self.fetchBalance()
             }
@@ -91,7 +109,8 @@ struct ContentView: View {
                 case .failure(let error): print("Error: \(error)")
                 }
             }, receiveValue: { balanceResponse in
-                self.balance = Double(balanceResponse.userId)
+                self.balance = Double(balanceResponse.balance)
+                self.bitcoinPrice = Double(balanceResponse.price)
                 self.lastUpdated = Date()
             })
     }
@@ -105,7 +124,8 @@ struct ContentView: View {
 }
 
 struct BalanceResponse: Decodable {
-    let userId: Int
+    let balance: Double
+    let price: Double
 }
 
 struct ContentView_Previews: PreviewProvider {
