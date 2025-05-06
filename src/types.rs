934b244350d7bf0419b67393e0b3750c84d16c7d
toneloc <toneloc@users.noreply.github.com>
2025-05-06 14:56:30 -0400

    changed btc formatting

--- src/types.rs
+++ src/types.rs
@@ -104,7 +104,7 @@ impl std::fmt::Display for Bitcoin {
             .map(|(i, c)| if i == 4 || i == 7 { format!(" {}", c) } else { c.to_string() })
             .collect::<String>();
 
-        write!(f, "{}btc", with_spaces)
+        write!(f, "{} BTC", with_spaces)
     }
 }
 
