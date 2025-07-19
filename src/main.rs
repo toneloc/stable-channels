f63fc0399554f843f04a08d11222b915cd7089fb
toneloc <@tonklaus>
2025-07-19 13:37:58 -0400

    removed debugs attrs

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,4 @@
-#[cfg_attr(all(target_os = "windows", not(debug_assertions)), windows_subsystem = "windows")]
+#![cfg_attr(target_os = "windows", windows_subsystem = "windows")]
 
 /// Stable Channels in LDK 
 /// Contents
