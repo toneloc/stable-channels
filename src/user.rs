adbe3dfc6aab2ebc8446223d2228e97a33dfa361
toneloc <toneloc@users.noreply.github.com>
2025-08-21 13:48:50 -0400

    Update user.rs for builds

--- src/user.rs
+++ src/user.rs
@@ -30,11 +30,11 @@
     const USER_PORT: u16 = 9736;
 
     // Populate the below two parameters to run locally
-    const DEFAULT_LSP_PUBKEY: &str = "02effe2d4284e8324e4f64bea853f0569c98a3c734e6b12f11afce28aa996fe9e9";
-    const DEFAULT_GATEWAY_PUBKEY: &str = "033b5445cd81840dcbe4dc9d2c8a043f120481506d28ac1fc9a512ddcc0dbbb49e";
+    const DEFAULT_LSP_PUBKEY: &str = "0388948c5c7775a5eda3ee4a96434a270f20f5beeed7e9c99f242f21b87d658850";
+    const DEFAULT_GATEWAY_PUBKEY: &str = "03da1c27ca77872ac5b3e568af30673e599a47a5e4497f85c7b5da42048807b3ed";
 
-    const DEFAULT_LSP_ADDRESS: &str = "54.210.112.22:9737";
-    const DEFAULT_GATEWAY_ADDRESS: &str = "127.0.0.1:9735";
+    const DEFAULT_LSP_ADDRESS: &str = "100.25.168.115:9737";
+    const DEFAULT_GATEWAY_ADDRESS: &str = "213.174.156.80:9735";
     const EXPECTED_USD: f64 = 100.0;
     const DEFAULT_CHAIN_SOURCE_URL: &str = "https://blockstream.info/api/";
 
